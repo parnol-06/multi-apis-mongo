882a459d3b61a12935d01c624b354af9eed6d29c
Parnol-06 <arnolferneyperez@hotmail.com>
2025-11-01 14:00:49 -0500

    Front 2

--- users-api/src/db.js
+++
@@ -1,6 +0,0 @@
-import pg from "pg";
-
-export const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Azure PG exige TLS; para demo deshabilitamos validación de CA
-});
